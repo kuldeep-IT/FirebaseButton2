3886019eb728b0b2d4d1d75083ff19d4cd8ee0c9
kuldeep-IT <rathod99244@gmail.com>
2023-05-22 15:02:46 +0530

    - network call with native HttpURLConnection

--- app/src/main/java/com/dada/firebasebutton2/MainActivity.java
+++ app/src/main/java/com/dada/firebasebutton2/MainActivity.java
@@ -2,15 +2,21 @@ package com.dada.firebasebutton2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import com.dada.firebasebutton.CustomButton;
+
+import com.tagmate.tatvic.CustomButton;
+import com.tagmate.tatvic.FirebaseAnalytics;
+
 
 public class MainActivity extends AppCompatActivity {
 
     CustomButton button;
+    FirebaseAnalytics firebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.fireBaseButton);
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Bundle bundle = new Bundle();
+        bundle.putString("KEY_1","VIEW_ITEM_1");
+
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.VIEW_ITEM,bundle);
+
+//        SharedPreferences sp = ((BaseApp) getApplicationContext()).getSharedPreferences();
+//        Log.d("ABC_XYZ", "onCreate: "+sp.getBoolean("SERVER_STATUS", false));
 
         button.setCustomClickListener(new CustomButton.CustomOnClickListener() {
             @Override
--- firebaseButton/src/main/java/com/tagmate/tatvic/CustomButton.java
+++ firebaseButton/src/main/java/com/tagmate/tatvic/CustomButton.java
@@ -1,4 +1,4 @@
-package com.dada.firebasebutton;
+package com.tagmate.tatvic;
 
 import android.content.Context;
 import android.os.Bundle;
